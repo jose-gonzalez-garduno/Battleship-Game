1e1aa65b23930c2140f6a5ac3a3ba5ca30a49893
Jose.Gonzalez <joseag1999@gmail.com>
2017-04-06 14:26:57 -0700

    Finishing the view object

--- battleship.js
+++ battleship.js
@@ -2,14 +2,32 @@ var view = {
   // this method takes a string message and displays it
   // in the message display area
   displayMessage: function (msg) {
-    //Get the ID from
-    var messageArea = document.getElementById("messageArea");
-    messageArea.innerHtml = msg;
+    //Get the ID from the message tag
+    var messageArea = document.getElementById('messageArea');
+    messageArea.innerHTML = msg;
   },
+
+  // take the location of a hit as an argument
   displayHit: function (location) {
-
+    // take the location of a miss as an argument
+    var cell = document.getElementById(location);
+    // Set class to 'hit' to change to the hit image
+    cell.setAttribute("class", "hit");
   },
-  displayMiss: function (location) {
 
+  // take the location of a miss as an argument
+  displayMiss: function (location) {
+    var cell = document.getElementById(location);
+    // Set class to 'miss' to change to the miss image
+    cell.setAttribute("class", "miss");
   }
 };
+
+// Testing the view object
+view.displayMiss("00");
+view.displayHit("34");
+view.displayHit("55");
+view.displayMiss("25");
+view.displayMiss("12");
+
+view.displayMessage("Test Test Test!");
